83a95d3de13e7db6c446c1a7343c79658cc2799d
Donny Arieffian <donny.arieffian@kuncie.com>
2023-02-13 12:53:06 +0700

    add database migration

--- database/migrations/2023_02_13_032018_create_product_image_table.php
+++ database/migrations/2023_02_13_032018_create_product_image_table.php
@@ -13,14 +13,9 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
-            $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+        Schema::create('product_image', function (Blueprint $table) {
+            $table->integer('product_id')->nullable(false);
+            $table->integer('image_id')->nullable(false);
         });
     }
 
@@ -31,6 +26,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('product_image');
     }
 };
--- database/migrations/2023_02_13_040511_create_product_image_index.php
+++ database/migrations/2023_02_13_040511_create_product_image_index.php
@@ -13,14 +13,9 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+        Schema::table('product_image', function (Blueprint $table) {
+            $table->index('product_id');
+            $table->index('image_id');
         });
     }
 
@@ -31,6 +26,9 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::table('product_image', function (Blueprint $table) {
+            $table->dropIndex('product_id');
+            $table->dropIndex('image_id');
+        });
     }
 };
