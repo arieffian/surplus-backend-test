6ff4ee383aa5681ff3eb4d62597c3c3b03bc69a1
Donny Arieffian <donny.arieffian@kuncie.com>
2023-02-13 18:15:30 +0700

    chore: remove unused import

--- app/Http/Controllers/CategoryControllers/CategoryListController.php
+++ app/Http/Controllers/CategoryControllers/CategoryListController.php
@@ -2,9 +2,6 @@
 
 namespace App\Http\Controllers\CategoryControllers;
 
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Foundation\Bus\DispatchesJobs;
-use Illuminate\Foundation\Validation\ValidatesRequests;
 use App\Http\Controllers\Controller as BaseController;
 use App\Models\Category;
 
