4dbfacaae799b466bbe41af9d40514baa336fa00
Donny Arieffian <donny.arieffian@kuncie.com>
2023-02-13 13:31:20 +0700

    feat: add models

---
+++ app/Models/Product.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+ 
+use Illuminate\Database\Eloquent\Model;
+ 
+class Product extends Model
+{
+    protected $table = 'products';
+    protected $fillable = ['name', 'description', 'enable'];
+ 
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class, 'category_product', 'product_id', 'category_id');
+    }
+
+    public function images()
+    {
+        return $this->belongsToMany(Image::class, 'product_image', 'product_id', 'image_id');
+    }
+}
